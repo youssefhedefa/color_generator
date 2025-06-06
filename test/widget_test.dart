53930b9bbaa6e118d862a9c8c27d529548f2f089
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2025-06-07 02:17:24 +0300

    Format test file.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,23 +4,30 @@ import 'package:test_task/color_generator_app.dart';
 
 void main() {
   group('ColorGeneratorApp Tests', () {
-    testWidgets('Background and text colors change on tap', (WidgetTester tester) async {
+    testWidgets('Background and text colors change on tap', (
+      WidgetTester tester,
+    ) async {
       // Build the app and trigger a frame.
       await tester.pumpWidget(const ColorGeneratorApp());
 
       // Verify the initial state of the app.
-      final initialContainer = tester.widget<AnimatedContainer>(find.byType(AnimatedContainer));
-      final initialBackgroundColor = (initialContainer.decoration as BoxDecoration).color;
+      final initialContainer = tester.widget<AnimatedContainer>(
+        find.byType(AnimatedContainer),
+      );
+      final initialBackgroundColor =
+          (initialContainer.decoration as BoxDecoration).color;
 
       // Simulate a tap on the GestureDetector.
       await tester.tap(find.byType(GestureDetector));
       await tester.pumpAndSettle();
 
       // Verify that the background color has changed.
-      final newContainer = tester.widget<AnimatedContainer>(find.byType(AnimatedContainer));
-      final newBackgroundColor = (newContainer.decoration as BoxDecoration).color;
+      final newContainer = tester.widget<AnimatedContainer>(
+        find.byType(AnimatedContainer),
+      );
+      final newBackgroundColor =
+          (newContainer.decoration as BoxDecoration).color;
       expect(newBackgroundColor, isNot(equals(initialBackgroundColor)));
-
     });
   });
 }
