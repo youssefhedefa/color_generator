3e4bfd9ff4b5512287d0471cba0449c8d2bcf42f
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2025-06-07 02:07:36 +0300

    Add test cases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test_task/color_generator_app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ColorGeneratorApp());
+  group('ColorGeneratorApp Tests', () {
+    testWidgets('Background and text colors change on tap', (WidgetTester tester) async {
+      // Build the app and trigger a frame.
+      await tester.pumpWidget(const ColorGeneratorApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Verify the initial state of the app.
+      final initialContainer = tester.widget<AnimatedContainer>(find.byType(AnimatedContainer));
+      final initialBackgroundColor = (initialContainer.decoration as BoxDecoration).color;
+      final initialTextColor = (tester.firstWidget(find.byType(AnimatedDefaultTextStyle)) as AnimatedDefaultTextStyle).style.color;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Simulate a tap on the GestureDetector.
+      await tester.tap(find.byType(GestureDetector));
+      await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Verify that the background color has changed.
+      final newContainer = tester.widget<AnimatedContainer>(find.byType(AnimatedContainer));
+      final newBackgroundColor = (newContainer.decoration as BoxDecoration).color;
+      expect(newBackgroundColor, isNot(equals(initialBackgroundColor)));
+
+    });
   });
 }
